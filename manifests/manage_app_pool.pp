f2619f6350a5053d120e0f0a4dcf822e8cc79875
lc8158 <lc8158@LCOULSON01D.intdom.bna.com>
2016-01-07 10:56:31 -0500

    app pool idle timeout

--- manifests/manage_app_pool.pp
+++ manifests/manage_app_pool.pp
@@ -1,20 +1,26 @@
-#
-define iis::manage_app_pool(
+define iis::manage_app_pool (
   $app_pool_name           = $title,
   $enable_32_bit           = false,
   $managed_runtime_version = 'v4.0',
   $managed_pipeline_mode   = 'Integrated',
   $ensure                  = 'present',
   $start_mode              = 'OnDemand',
-  $rapid_fail_protection   = true
-){
-
+  $rapid_fail_protection   = true,
+  $apppoolidletimeoutminutes
+  ) {
   validate_bool($enable_32_bit)
   validate_re($managed_runtime_version, ['^(v2\.0|v4\.0)$'])
   validate_re($managed_pipeline_mode, ['^(Integrated|Classic)$'])
   validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
   validate_re($start_mode, '^(OnDemand|AlwaysRunning)$')
   validate_bool($rapid_fail_protection)
+
+if $apppoolidletimeoutminutes != undef {
+  validate_integer($apppoolidletimeoutminutes, 43200, 0) #30 days (43200 min) is max value for this in iis, 0 disables
+  $processAppPoolIdleTimeout = true
+  $idleTimeoutTicks = $apppoolidletimeoutminutes * 600000000
+}
+else{$processAppPoolIdleTimeout = false}
 
   if ($ensure in ['present','installed']) {
     exec { "Create-${app_pool_name}" :
@@ -69,8 +75,20 @@ define iis::manage_app_pool(
       require   => Exec["Create-${app_pool_name}"],
       logoutput => true,
     }
-  } else {
-    exec { "Delete-${app_pool_name}" :
+
+  if($processAppPoolIdleTimeout)
+  {
+        exec { "App Pool Idle Timeout - ${app_pool_name}":
+        command   => "Import-Module WebAdministration;\$appPoolPath = (\"IIS:\\AppPools\\\" + \"${app_pool_name}\");[TimeSpan]\$ts = ${idleTimeoutTicks};Set-ItemProperty \$appPoolPath -name processModel -value @{idletimeout=\$ts}",
+        provider  => powershell,
+        unless    => "Import-Module WebAdministration;\$appPoolPath = (\"IIS:\\AppPools\\\" + \"${app_pool_name}\");[TimeSpan]\$ts = ${idleTimeoutTicks};if((get-ItemProperty \$appPoolPath -name processModel.idletimeout.value) -ne \$ts){exit 1;}exit 0;",
+        require   => Exec["Create-${app_pool_name}"],
+        logoutput => true,
+      }
+  }
+    }
+ else {
+    exec { "Delete-${app_pool_name}":
       command   => "Import-Module WebAdministration; Remove-Item \"IIS:\\AppPools\\${app_pool_name}\" -Recurse",
       provider  => powershell,
       onlyif    => "Import-Module WebAdministration; if(!(Test-Path \"IIS:\\AppPools\\${app_pool_name}\")) { exit 1 } else { exit 0 }",
